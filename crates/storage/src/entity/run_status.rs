040e2b3fd88dd60df70984ccb60ba33461b9cd09
Alex Acebo <aacebowork@gmail.com>
2026-04-06 09:16:00 -0700

    Update run_status.rs

--- crates/storage/src/entity/run_status.rs
+++ crates/storage/src/entity/run_status.rs
@@ -2,7 +2,6 @@
 #[sqlx(type_name = "TEXT", rename_all = "snake_case")]
 pub enum RunStatus {
     Pending,
-    #[sqlx(rename = "in-progress")]
     InProgress,
     Done,
 }
@@ -11,7 +10,7 @@ impl std::fmt::Display for RunStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Pending => write!(f, "pending"),
-            Self::InProgress => write!(f, "in-progress"),
+            Self::InProgress => write!(f, "in_progress"),
             Self::Done => write!(f, "done"),
         }
     }
